20d8e181944a8cd5cf2b97aceb13af92657d7177
soonzero <sykeum0912@naver.com>
2023-04-07 16:52:18 +0900

    :tada: 15829

--- 15829.js
+++ 15829.js
@@ -2,9 +2,13 @@ let fs = require('fs');
 let [L, string] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let sum = 0;
+let r = 1;
 
 for (let i = 0; i < L; i++) {
-	sum += (string[i].charCodeAt() - 96) * 31 ** i;
+	sum += (string[i].charCodeAt() - 96) * r;
+	r *= 31;
+	r %= 1234567891;
+	sum %= 1234567891;
 }
 
 console.log(sum);
