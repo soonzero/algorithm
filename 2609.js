7c797f6a450435304b1180b31e6638a9b8178c75
soonzero <sykeum0912@naver.com>
2023-04-08 17:42:53 +0900

    :tada: 2609

---
+++ 2609.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const [M, N] = fs
+	.readFileSync('test.txt')
+	.toString()
+	.trim()
+	.split(' ')
+	.map(Number)
+	.sort((a, b) => a - b);
+
+// 시간 오래 걸림 => 780ms 정도
+
+let max = 1;
+let min = M * N;
+
+for (let i = 1; i < M * N; i++) {
+	if (i <= Math.min(M, N) && M % i === 0 && N % i === 0) max = i;
+
+	if (i % M === 0 && i % N === 0) {
+		min = i;
+		break;
+	}
+}
+
+console.log(`${max}\n${min}`);
