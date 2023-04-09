64c9bd8778304b4cd546b211dea1a11acb5e4712
soonzero <sykeum0912@naver.com>
2023-04-09 19:10:11 +0900

    :tada: 2609 - 시간 단축

--- 2609.js
+++ 2609.js
@@ -9,16 +9,26 @@ const [M, N] = fs
 
 // 시간 오래 걸림 => 780ms 정도
 
-let max = 1;
-let min = M * N;
+// let max = 1;
+// let min = M * N;
 
-for (let i = 1; i < M * N; i++) {
-	if (i <= Math.min(M, N) && M % i === 0 && N % i === 0) max = i;
+// for (let i = 1; i < M * N; i++) {
+// 	if (i <= Math.min(M, N) && M % i === 0 && N % i === 0) max = i;
 
-	if (i % M === 0 && i % N === 0) {
-		min = i;
-		break;
-	}
+// 	if (i % M === 0 && i % N === 0) {
+// 		min = i;
+// 		break;
+// 	}
+// }
+
+// console.log(`${max}\n${min}`);
+
+// M * N / 최대 공약수 = 최소 공배수
+// 124ms
+
+function getGCD(M, N) {
+	if (M % N === 0) return N;
+	else return getGCD(N, M % N);
 }
 
-console.log(`${max}\n${min}`);
+console.log(`${getGCD(M, N)}\n${(M * N) / getGCD(M, N)}`);
