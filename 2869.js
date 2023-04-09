71f31ca00b4fabcd102dd9cbad844f9d01110b57
soonzero <sykeum0912@naver.com>
2023-04-09 19:21:17 +0900

    :tada: 2869

--- 2869.js
+++ 2869.js
@@ -6,14 +6,21 @@ let [A, B, V] = fs
 	.split(' ')
 	.map(Number);
 
-let dayCount = 0;
-let height = 0;
+// 시간 초과 - 반복문 사용 x
 
-while (true) {
-	dayCount++;
-	height += A;
-	if (height >= V) break;
-	height -= B;
-}
+// let dayCount = 0;
+// let height = 0;
 
-console.log(dayCount);
+// while (true) {
+// 	dayCount++;
+// 	height += A;
+// 	if (height >= V) break;
+// 	height -= B;
+// }
+
+// console.log(dayCount);
+
+// 낮 동안에 목표 높이까지 올라가기만 해도 성공할 수 있기 때문에
+// 나눌 수에서 미끄러질 높이를 빼줘야 함
+
+console.log(Math.ceil(V - B) / (A - B));
