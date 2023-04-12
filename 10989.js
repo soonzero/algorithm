ccddaaaf3541abdc5a7bffc5edf69f10089af909
soonzero <sykeum0912@naver.com>
2023-04-12 23:38:48 +0900

    :fire: 10989 - 메모리 초과

---
+++ 10989.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let [, ...numbers] = fs
+	.readFileSync('/dev/stdin')
+	.toString()
+	.trim()
+	.split('\n')
+	.map(Number);
+
+console.log(
+	numbers
+		.sort((a, b) => a - b)
+		.join('\n')
+		.trim()
+);
