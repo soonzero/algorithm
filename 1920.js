c28a043153684745642c68a95507e48d1895f199
soonzero <sykeum0912@naver.com>
2023-04-18 23:51:56 +0900

    :tada: 1920

--- 1920.js
+++ 1920.js
@@ -1,17 +1,29 @@
 const fs = require('fs');
-const [N, scope, M, numbers] = fs
+const [, scope, , numbers] = fs
 	.readFileSync('/dev/stdin')
 	.toString()
 	.trim()
 	.split('\n');
 
-let answer = [];
+// 1차 시도 - 메모리 초과
+// let answer = [];
 
-for (let i = 0; i < Number(M); i++) {
-	let numArray = numbers.split(' ').map(Number);
-	let scopeArray = scope.split(' ').map(Number);
-	if (scopeArray.includes(numArray[i])) answer.push(1);
-	else answer.push(0);
-}
+// for (let i = 0; i < Number(M); i++) {
+// 	let numArray = numbers.split(' ').map(Number);
+// 	let scopeArray = scope.split(' ').map(Number);
+// 	if (scopeArray.includes(numArray[i])) answer.push(1);
+// 	else answer.push(0);
+// }
 
-console.log(answer.join('\n').trim());
+// console.log(answer.join('\n').trim());
+
+// 배열 말고 set으로 사용하니까 됨
+
+let scopeSet = new Set(scope.split(' '));
+
+console.log(
+	numbers
+		.split(' ')
+		.map((number) => (scopeSet.has(number) ? 1 : 0))
+		.join('\n')
+);
