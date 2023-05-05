f440a13bc9dbd68c5f89a35169d8fb51f2819386
soonzero <sykeum0912@naver.com>
2023-05-05 22:38:54 +0900

    :tada: 18111

--- 18111.js
+++ 18111.js
@@ -2,25 +2,23 @@ const fs = require('fs');
 let inputs = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [N, M, B] = inputs.shift().split(' ').map(Number);
-const land = [];
-
-for (let i = 0; i < N; i++) {
-	land.push(...inputs.map((item) => item.split(' ').map(Number))[i]);
-}
+const land = inputs.map((item) => item.split(' ').map(Number));
 
 const getTime = (height) => {
 	let blocks = B;
 	let removal = 0;
 	let added = 0;
 
-	for (let i = 0; i < N * M; i++) {
-		const diff = land[i] - height;
-		if (diff > 0) {
-			removal += diff;
-			blocks += diff;
-		} else if (diff < 0) {
-			added += -diff;
-			blocks -= -diff;
+	for (let i = 0; i < N; i++) {
+		for (let j = 0; j < M; j++) {
+			const diff = land[i][j] - height;
+			if (diff > 0) {
+				removal += diff;
+				blocks += diff;
+			} else if (diff < 0) {
+				added += -diff;
+				blocks -= -diff;
+			}
 		}
 	}
 	if (blocks < 0) return [Infinity, height];
